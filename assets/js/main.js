9557ddd3bc5f03f0d4a0ffca04ea2cab06b74077
Calvin Magezi <30309553+CalvinMagezi@users.noreply.github.com>
2021-07-06 12:54:26 +0300

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,6 +98,8 @@
     }
   });
 
+
+
   // Navigation active state on scroll
   var nav_sections = $("section");
   var main_nav = $(".nav-menu, .mobile-nav");
@@ -142,6 +144,42 @@
       "easeInOutExpo"
     );
     return false;
+  });
+
+  /**
+   * Clients Slider
+   */
+   new Swiper('.clients-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 60
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 80
+      },
+      992: {
+        slidesPerView: 6,
+        spaceBetween: 120
+      }
+    }
   });
 
   // jQuery counterUp
